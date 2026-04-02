13e480d67493f78e4a2c90a6a7dfc2d66c12f184
felipe-071 <felipefelxg@gmail.com>
2026-04-02 20:38:23 -0300

    First commit

---
+++ main.py
@@ -0,0 +1,9 @@
+def sum(a, b):
+    return a + b
+
+num1 = int(input("Insira o primeiro número para a soma: "))
+num2 = int(input("Insira o segundo número para a soma: "))
+
+result = sum(num1, num2)
+
+print(f"O resultado de {num1} + {num2} é {result}")
